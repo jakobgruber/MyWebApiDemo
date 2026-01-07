16d6615135018d6d1e8382b24674b2f9461009fe
Jakob Gruber <der@jakobgruber.at>
2026-01-07 07:39:18 +0100

    added data annotations

--- MyDemo.WebApi/Contracts/Login.cs
+++ MyDemo.WebApi/Contracts/Login.cs
@@ -1,8 +1,19 @@
-﻿namespace MyDemo.WebApi.Contracts;
+﻿using MyDemo.WebApi.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyDemo.WebApi.Contracts;
 
 public class LoginRequest
 {
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string Claim { get; set; } = string.Empty;
+    [Required]
+    [MinLength(1)]
+    public string Username { get; init; } = string.Empty;
+
+    [Required]
+    [MinLength(7)]
+    public string Password { get; init; } = string.Empty;
+
+    [Required]
+    [AllowedValues(Roles.Admin, Roles.User, ErrorMessage = $"Claim must be {Roles.Admin} or {Roles.User}.")]
+    public string Claim { get; init; } = string.Empty;
 }
